09d21a93657089a4f78ad6b5f44c645bcc5d1c60
Justin Robertson <justinr@uprate.co.za>
2021-08-14 20:39:42 +0200

    added all mongo event files

--- service/event.mongo.inserted.go
+++ service/event.mongo.inserted.go
@@ -10,5 +10,5 @@ func init() {
 }
 
 func eventMongoInserted(r uniform.IRequest, p diary.IPage) {
-	// todo: react based on which database and collection has been updated
+	// todo: react based on which database and collection has been inserted
 }
